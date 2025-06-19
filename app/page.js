cce6df9f827567564773187fa833ff5e4e82f5fa
jacobosanchezbrito <sbjacobo4@gmail.com>
2025-06-19 12:17:23 -0500

    Configuracion inicial del frontend, desde Visual.

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Button from '../components/Button';
+
+export default function HomePage() {
+  return (
+    <div className="p-10">
+      <Button>Click Aquí</Button>
+    </div>
+  );
+}
+
+
